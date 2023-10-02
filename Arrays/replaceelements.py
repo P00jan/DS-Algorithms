1a888e6c6ce5c6d607ea767f26e426e6595e8e75
P00jan <poojan2132000p@gmail.com>
2023-10-02 15:36:06 -0400

    replaceElements-py

--- Arrays/replaceelements.py
+++ Arrays/replaceelements.py
@@ -14,3 +14,15 @@ print(f"The replaced elements are: {result1}")
 arr2 = [1,2,14,19,5,4,8,4,2]
 result2 = Solution.replaceElements(arr2)
 print(f"The replaced elements are: {result2}")
+
+"""
+Output:
+The replaced elements are: [18, 6, 6, 6, 1, -1]
+The replaced elements are: [19, 19, 19, 8, 8, 8, 4, 2, -1]
+
+
+Explanation:
+We are assigning the max num with -1 and we start from reverse end as we always have to check 1 element if we 
+start from beginning we have to check all the elements in the array, the logic is simple if the given element in array is bigger than the element 
+we stored in temp variable it will replace it in temp and in array.
+"""
